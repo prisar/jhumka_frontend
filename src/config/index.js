a1c2089c9bb96f293e078569efae3f650c9dc3c9
prisar <pritimaysarkar2010@gmail.com>
2019-08-13 04:04:07 +0530

    change url

--- src/config/index.js
+++ src/config/index.js
@@ -2,14 +2,14 @@
  * App configurations
  */
 
-export const API_URL = 'http://localhost:8000/api/';
-export const API_ROOT = 'http://localhost:8000/';
+// export const API_URL = 'http://localhost:8000/api/';
+// export const API_ROOT = 'http://localhost:8000/';
 
 // export const API_URL = 'https://intense-ocean-48593.herokuapp.com/api/';
 // export const API_ROOT = 'https://intense-ocean-48593.herokuapp.com/';
 
-// export const API_URL = 'https://cryptic-shore-38266.herokuapp.com/api/';
-// export const API_ROOT = 'https://cryptic-shore-38266.herokuapp.com/';
+export const API_URL = 'https://cryptic-shore-38266.herokuapp.com/api/';
+export const API_ROOT = 'https://cryptic-shore-38266.herokuapp.com/';
 
 export const META = {
   PAGE_TITLE_SUFFIX: 'Jhumka Web App',
